0981c61474b6db778ac1f9fd92a0cae03029c6d0
kate-makarova <kate.makarova@hotmail.com>
2025-12-30 19:29:57 -0500

    init

---
+++ src/app/navlinks/navlinks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navlinks',
+  imports: [],
+  templateUrl: './navlinks.component.html',
+  styleUrl: './navlinks.component.css'
+})
+export class NavlinksComponent {
+
+}
