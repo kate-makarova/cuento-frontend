50840a882542b1fd986ea3aedd42fbe0ce7950ab
viper-f <143018173+viper-f@users.noreply.github.com>
2026-09-12 10:47:59 -0400

    Install locale: ch-Ch

--- src/locale_config.ts
+++ src/locale_config.ts
@@ -1,6 +1,12 @@
 export interface LocaleDefinition {
   code: string;
-  langPrefixes: string[];
+  langPrefixes: string[  {
+    code: 'ch-Ch',
+    langPrefixes: ['ch'],
+    translations: () => import('./locale/ch').then(m => m.TRANSLATIONS_CH),
+    angularLocale: () => import('@angular/common/locales/ch'),
+  },
+];
   translations: () => Promise<Record<string, string>>;
   angularLocale: () => Promise<any>;
 }
