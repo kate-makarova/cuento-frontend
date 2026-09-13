127a0d25cc856bea005ef39cb10c101ed8437829
lylaiomai-spec <lylaiomai@gmail.com>
2026-09-13 14:07:54 +0200

    Install locale: ru-RU

---
+++ src/locale_config.ts
@@ -0,0 +1,23 @@
+export interface LocaleDefinition {
+  code: string;
+  langPrefixes: string[];
+  translations: () => Promise<Record<string, string>>;
+  angularLocale?: string;
+}
+
+export const LOCALES: LocaleDefinition[] = [
+  {
+    code: 'ru-RU',
+    langPrefixes: ['ru'],
+    translations: () => import('./locale/ru').then(m => m.TRANSLATIONS_RU),
+    angularLocale: 'ru',
+  },
+  {
+    code: 'ru-RU',
+    langPrefixes: ['ru'],
+    translations: () => import('./locale/ru').then(m => m.TRANSLATIONS_RU),
+  angularLocale: 'ru',
+  },
+];
+
+export const DEFAULT_LOCALE = 'en-CA';
