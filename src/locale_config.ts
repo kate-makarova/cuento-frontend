8b6aaebcfd4a765a22a96d804a4b4bdba95e6434
lylaiomai-spec <lylaiomai@gmail.com>
2026-09-13 14:29:18 +0200

    Uninstall locale: ru-RU

--- src/locale_config.ts
+++ src/locale_config.ts
@@ -6,18 +6,6 @@ export interface LocaleDefinition {
 }
 
 export const LOCALES: LocaleDefinition[] = [
-  {
-    code: 'ru-RU',
-    langPrefixes: ['ru'],
-    translations: () => import('./locale/ru').then(m => m.TRANSLATIONS_RU),
-    angularLocale: 'ru',
-  },
-  {
-    code: 'ru-RU',
-    langPrefixes: ['ru'],
-    translations: () => import('./locale/ru').then(m => m.TRANSLATIONS_RU),
-  angularLocale: 'ru',
-  },
 ];
 
 export const DEFAULT_LOCALE = 'en-CA';
