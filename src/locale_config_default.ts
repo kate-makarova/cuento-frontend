9a5f8eb44c193f92426734ebb64aeebb7da1a60a
kate-makarova <54686234+kate-makarova@users.noreply.github.com>
2026-09-16 21:59:03 -0400

    Add Russian locale definition to LOCALES

--- src/locale_config_default.ts
+++ src/locale_config_default.ts
@@ -5,6 +5,13 @@ export interface LocaleDefinition {
   angularLocale?: string;
 }
 
-export const LOCALES: LocaleDefinition[] = [];
+export const LOCALES: LocaleDefinition[] = [
+  {
+    code: 'ru-RU',
+    langPrefixes: ['ru'],
+    translations: () => import('./locale/ru').then(m => m.TRANSLATIONS_RU),
+    angularLocale: 'ru',
+  },
+];
 
 export const DEFAULT_LOCALE = 'en-CA';
