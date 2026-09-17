d814a92376c0706fb42dd002f73b665bfd212a79
viper-f <143018173+viper-f@users.noreply.github.com>
2026-09-16 22:58:15 -0400

    Install locale: ru-RU

--- src/locale_config.ts
+++ src/locale_config.ts
@@ -6,6 +6,13 @@ export interface LocaleDefinition {
 }
 
 export const LOCALES: LocaleDefinition[
-] = [];
+] = [
+  {
+    code: 'ru-RU',
+    langPrefixes: ['ru'],
+    translations: () => import('./locale/ru').then(m => m.TRANSLATIONS_RU),
+  angularLocale: 'ru',
+  },
+];
 
 export const DEFAULT_LOCALE = 'en-CA';
